9b7a973099f2963863bcb7bd3d5c3d81a72cdbdc
LuffyxZx <nmk1195@gmail.com>
2026-03-27 14:11:06 +0700

    db

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
   ssl: {}
 });
 
-module.exports = connection;
+module.exports = pool;
